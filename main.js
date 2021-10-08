b9859c158c99255ce7030a96a8fd5d8062d17c61
hoddiemaster <79085243+hoddiemaster@users.noreply.github.com>
2021-10-08 17:04:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+mustache = "downdload.png";
+lips = "lips.png";
+
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+}
+
+function draw() {
+
+}
+
+function filter() {
+    mustache = "downdload.png";
+    lips = "lips.png";
+
+}
